6aad9a6410f1425936e078708b94b8ff374f21ba
aiXing <862099730@qq.com>
2015-11-20 18:39:01 +0800

    ...

---
+++ AXImagePickerController/AXImagePickerController/AXAssetsImageController.m
@@ -0,0 +1,23 @@
+//
+//  AXAssetsImageController.m
+//  AXSwift2OC
+//
+//  Created by ai on 9/8/15.
+//  Copyright © 2015 ai. All rights reserved.
+//
+
+#import "AXAssetsImageController.h"
+#import "PHAsset+Image.h"
+#import "ALAsset+Image.h"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated"
+@implementation AXAssetsImageController
++ (instancetype)defaultControllerWithAsset:(id)asset {
+    AXAssetsImageController *controller = [[AXAssetsImageController alloc] init];
+    controller.asset = asset;
+    if (![asset isKindOfClass:[PHAsset class]] && ![asset isKindOfClass:[ALAsset class]]) return nil;
+    controller.imageView.image = [asset valueForKey:@"image"];
+    return controller;
+}
+@end
+#pragma clang diagnostic pop
